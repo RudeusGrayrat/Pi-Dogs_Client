068ccf393e0b19383c477b84a923cdf472da863f
RudeusGrayrat <miguelnc2502@gmail.com>
2024-03-28 03:03:57 -0500

    media screen

--- cliente/src/components/Landing/Landing.jsx
+++ cliente/src/components/Landing/Landing.jsx
@@ -1,21 +1,22 @@
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import styles from "./Landing.module.css";
 
 function Landing() {
-    
+
   return (
     <div >
       <div className={styles.bienvenido}>
         <NavLink to="/home" className={styles.navlink}>
-        <h1 className={styles.pokemons}>
-          DOGS
-        </h1 >
+          <h1 className={styles.pokemons}>
+            DOGS
+          </h1 >
         </NavLink>
         <p className={styles.nc}>
           create by @nc
         </p>
 
       </div>
+      <footer>"RudeusGrayrat" en github</footer>
     </div>
   );
 }
